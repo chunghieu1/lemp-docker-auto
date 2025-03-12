4509c797d113062c556fda3b129d2cb15930cca5
hieukhocnhe <trantrunghieu422@gmail.com>
2025-03-12 10:54:06 +0700

    Update install-lemp.sh file

--- test.sh
+++ test.sh
@@ -18,7 +18,7 @@ sudo apt install -y certbot
 # Create directory if it doesn't exist
 INSTALL_DIR=~/lemp-docker
 if [ ! -d "$INSTALL_DIR" ]; then
-    mkdir -p $INSTALL_DIR/{src,nginx,mysql}
+    mkdir -p $INSTALL_DIR/{src,nginx,mysql,certs}
 fi
 cd $INSTALL_DIR
 
@@ -76,13 +76,13 @@ services:
     image: phpmyadmin/phpmyadmin:latest
     container_name: phpmyadmin
     environment:
-      PMA_HOST: mysql
+      PMA_HOST: mysql_server
       MYSQL_ROOT_PASSWORD: root
       PMA_ABSOLUTE_URI: https://$(curl -s -4 ifconfig.me):8081/
     ports:
       - "8081:80"
     depends_on:
-      - mysql
+      - mysql_server
     networks:
       - lemp_network
 
@@ -99,6 +99,32 @@ cat <<EOF > nginx/default.conf
 server {
     listen 80;
     server_name $(curl -s -4 ifconfig.me);
+
+    root /var/www/html;
+    index index.php index.html index.htm;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    location ~ \.php\$ {
+        include fastcgi_params;
+        fastcgi_pass php:9000;
+        fastcgi_index index.php;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+    }
+
+    location ~ /\.ht {
+        deny all;
+    }
+}
+
+server {
+    listen 443 ssl;
+    server_name $(curl -s -4 ifconfig.me);
+
+    ssl_certificate /etc/nginx/certs/fullchain.pem;
+    ssl_certificate_key /etc/nginx/certs/privkey.pem;
 
     root /var/www/html;
     index index.php index.html index.htm;
@@ -146,6 +172,6 @@ fi
 
 echo "✅ Setup complete! Access your server:"
 echo "- PHP Info: http://$(curl -s -4 ifconfig.me)"
-echo "- phpMyAdmin: http://$(curl -s -4 ifconfig.me):8081"
+echo "- phpMyAdmin: https://$(curl -s -4 ifconfig.me):8081"
 echo "  - Username: root"
 echo "  - Password: root"
